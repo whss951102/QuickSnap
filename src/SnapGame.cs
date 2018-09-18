b48228d929dadd4b87f15f65b5f8672a273caa5d
jasperwong31 <100076014@students.swinburne.edu.my>
2018-09-18 14:30:42 -0700

    * CardGames.csproj: Change Font
    * SnapGame.cs:
    * Chunkfive.otf:

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -60,14 +60,16 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White,"GameFont", 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White,"GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White,"GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
+				SwinGame.DrawText ("No card played yet...", Color.White,"GameFont", 0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck
