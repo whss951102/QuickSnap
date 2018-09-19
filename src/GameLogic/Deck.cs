7c712adf089108d677e190b30b5e4dfc3fde0d3e
jongchunchen <chunchenjong@gmail.com>
2018-09-19 09:54:16 +0800

    update

--- src/GameLogic/Deck.cs
+++ src/GameLogic/Deck.cs
@@ -63,7 +63,7 @@ namespace CardGames.GameLogic
 			Random rnd = new Random ();
 			//for each card (no need to shuffle last card)
 
-			for(int i = 0; i > 52-1 ; i--) 
+			for(int i = 0; i < 52-1 ; i++) 
 			{
 				int rndIdx = rnd.Next (52 - i);
 
